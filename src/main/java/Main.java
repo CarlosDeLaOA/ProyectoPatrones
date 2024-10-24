8fdaf49e3931edde2c4a125eb1e5dd6e88f9d033
Key <kmoralesq06@gmail.com>
2024-10-23 23:36:19 -0600

    Muevo carpetas para que sirva el pom y agrego el target a gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)throws IOException {
