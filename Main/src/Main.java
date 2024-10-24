b9e245aebae4cbb4ef4ce76b7e96fc444860b1e7
Key <kmoralesq06@gmail.com>
2024-10-23 23:29:42 -0600

    Agrego conexion al API, creacion del pom y agrego libreria OkHttpClient

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,5 +1,30 @@
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("ingrese la pelicula");
+        String movie = in.readLine();
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.themoviedb.org/3/search/movie?query="+movie)
+                .get()
+                .addHeader("accept", "application/json")
+                .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNzVkYzIyNzI3NmU0YWQ4NDY4OGI2MzQ1NTQ2MDZlZiIsIm5iZiI6MTcyOTc0NDc2MS45NjkyODksInN1YiI6IjY3MTljZDg4MjY4NWNiNjU2M2MwOTZiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SewK_9MKll2KoAcxHzEnx2STJt89uRc7TYAqfsdB_JQ")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+
+
+
     }
 }
