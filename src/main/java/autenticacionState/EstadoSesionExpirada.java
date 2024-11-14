1ea5ab51aa2dd185b2dbd00c8dbc9a2d2838727e
Key <kmoralesq06@gmail.com>
2024-11-13 21:13:14 -0600

    Implementacion de la logica de EstadoSesionExpirada

--- src/main/java/autenticacionState/EstadoSesionExpirada.java
+++ src/main/java/autenticacionState/EstadoSesionExpirada.java
@@ -4,16 +4,22 @@ public class EstadoSesionExpirada implements EstadoAutenticacion{
 
     @Override
     public void iniciarSesion() {
+        System.out.println("Porfavor, vuelva a iniciar sesión");
+        ContextoAutenticacion.getInstance().setEstadoAutenticacion(new EstadoNoAutenticado());
 
     }
 
     @Override
     public void accederServicio() {
+        System.out.println("Sesion expirada");
+        ContextoAutenticacion.getInstance().setEstadoAutenticacion(new EstadoNoAutenticado());
 
     }
 
     @Override
     public void cerrarSesion() {
+        System.out.println("Sesion cerrada");
+        ContextoAutenticacion.getInstance().setEstadoAutenticacion(new EstadoNoAutenticado());
 
     }
 
