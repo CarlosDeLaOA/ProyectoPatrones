3923a763936db45cee277a1278439356d2d86b90
SammyToruno11 <samytoruno19@gmail.com>
2024-12-02 22:05:54 -0600

    StreamingServiceDecorator finalizado

--- src/main/java/decorator/StreamingServiceDecorator.java
+++ src/main/java/decorator/StreamingServiceDecorator.java
@@ -6,26 +6,40 @@ import strategy.SearchStrategy;
 
 import java.util.ArrayList;
 
-
+/**
+ * Clase decoradora para StreamingService.
+ * Esta clase permite agregar funcionalidades adicionales al servicio de streaming.
+ */
 public abstract class StreamingServiceDecorator extends StreamingService {
+/**
+* El servicio de streaming al que se le añaden funcionalidades adicionales.
+*/
     protected StreamingService servicio;
 
     public StreamingServiceDecorator(StreamingService servicio) {
         this.servicio = servicio;
     }
-
+/**
+* Configura el servicio de streaming base con los parámetros proporcionados.
+*/
     @Override
     public void configurar(ArrayList<String> configParams) {
         servicio.configurar(configParams);
     }
 
+/**
+* Realiza una consulta utilizando el servicio de streaming base.
+*/ 
     @Override
-    public ArrayList<SearchResult> consultar(String query, ArrayList<String> configParams, SearchStrategy searchStrategy) {
-        return servicio.consultar(query, configParams, searchStrategy);
+    public ArrayList<SearchResult> consultar(String query, ArrayList<String> configParams) {
+        return servicio.consultar(query, configParams);
     }
 
+/**
+* Realiza una busqueda utilizando el servicio de streaming base.
+*/ 
     @Override
-    public ArrayList<SearchResult> buscar(String query, ArrayList<String> configParams, SearchStrategy searchStrategy) {
-        return servicio.buscar(query, configParams, searchStrategy);
+    public ArrayList<SearchResult> buscar(String query, ArrayList<String> configParams) {
+        return servicio.buscar(query, configParams);
     }
 }
