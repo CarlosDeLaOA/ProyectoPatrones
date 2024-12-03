443590fc69bfd6b37724fcfe4e9364edd82d67c0
SammyToruno11 <samytoruno19@gmail.com>
2024-12-02 22:05:06 -0600

    LogDecorator finalizado

--- src/main/java/decorator/LogDecorator.java
+++ src/main/java/decorator/LogDecorator.java
@@ -3,35 +3,56 @@ package decorator;
 import search.Prototype;
 import search.SearchResult;
 import search.StreamingService;
-import strategy.SearchStrategy;
-
 import java.util.ArrayList;
 
-
+/**
+ * Decorador que agrega registro de logs al servicio de streaming.
+ */
 public class LogDecorator extends StreamingService {
+
+/**
+ *  Servicio base que se envuelve con funcionalidad adicional.
+ */
     private StreamingService servicioBase;
 
+/**
+ *  Constructor que inicializa el decorador con un servicio de streaming base.
+ */
     public LogDecorator(StreamingService servicioBase) {
         this.servicioBase = servicioBase;
     }
 
+/**
+ *  Configura el servicio base con los parámetros especificados y registra la acción.
+ */
     @Override
     public void configurar(ArrayList<String> configParams) {
         System.out.println("Registrando acción: Configuración del servicio.");
         servicioBase.configurar(configParams);
     }
 
+/**
+ * Realiza una consulta en el servicio base y registra la acción.
+ */
     @Override
-    public ArrayList<SearchResult> consultar(String query, ArrayList<String> configParams, SearchStrategy searchStrategy) {
+    public ArrayList<SearchResult> consultar(String query, ArrayList<String> configParams) {
         System.out.println("Registrando acción: Consulta de servicio.");
-        return servicioBase.consultar(query, configParams, searchStrategy);
+        return servicioBase.consultar(query, configParams);
     }
 
+/**
+ * Realiza una búsqueda en el servicio base y registra la acción.
+ */
+
     @Override
-    public ArrayList<SearchResult> buscar(String query, ArrayList<String> configParams, SearchStrategy searchStrategy) {
+    public ArrayList<SearchResult> buscar(String query, ArrayList<String> configParams) {
         System.out.println("Registrando acción: Búsqueda en servicio.");
-        return servicioBase.buscar(query, configParams, searchStrategy);
+        return servicioBase.buscar(query, configParams);
     }
+
+/**
+ * Crea una copia del decorador, incluyendo una copia del servicio base.
+ */
 
     @Override
     public Prototype clone() {
